d822b0f70f7da7503cf1037c1d1cb82f8217c0fe
sagacious-shell-7968 <tommysytc.tc@gmail.com>
2020-12-28 17:46:43 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -1,39 +1,90 @@
 
-counter=document.getElementById('counter');
+document.addEventListener("DOMContentLoaded",function(){
+    counterInc()
 
-let timeoutID = window.
-inc=function(){return setInterval (function(){let b=parseInt(counter.innerText);
-counter.innerText=b+1;},1*1000)};
-interval=inc();
- minus=document.getElementById("minus"),
- plus=document.getElementById("plus"),
- heart=document.getElementById("heart"),
- pause=document.getElementById("pause"),
- commentForm=document.getElementsByTagName("form")[0];
-  minus.addEventListener("click",function(){
-    b=parseInt(counter.innerText);
-    counter.innerText=b-1}),
-    
-  plus.addEventListener("click",function(){
-    b=parseInt(counter.innerText);
-    counter.innerText=b+1});
-    
- heart.addEventListener("click",function(){
-        var a=document.getElementById("counter"),
-        b=parseInt(a.innerText),
-        c=document.querySelector(".likes"),
-        d=void 0;
-        if([].concat(_toConsumableArray(c.children)).map(function(a){
-          return parseInt(a.dataset.num)}).includes(b)){d=document.querySelector('[data-num="'+b+'"]');
-          var e=parseInt(d.children[0].innerText);
-          d.innerHTML=b+" has been liked <span>"+(e+1)+"</span> times"}
-          else(d=document.createElement("li")).setAttribute("data-num",b),
-          d.innerHTML=b+" has been liked <span>1</span> time",
-          c.appendChild(d)});
-          
-          pause.addEventListener("click",function(){
-            window.clearInterval(interval);
-            pause.innertext="resume";
-          })
-          
-    
+})
+const counter = document.querySelector("#counter")
+const plus = document.querySelector("#plus")
+const minus = document.querySelector("#minus")
+const heart = document.querySelector("#heart")
+const pause = document.querySelector("#pause")
+const commentForm = document.querySelector("#comment-form")
+const comments = document.querySelector(".comments")
+const submit = document.querySelector("#submit")
+
+
+let pauseBtn = false
+let flag = false
+let timer = setInterval(counterInc,1000)
+
+
+plus.addEventListener("click",plusCounter)
+minus.addEventListener("click",minusCounter)
+heart.addEventListener("click",likesCounter)
+pause.addEventListener("click",pauseEverything)
+submit.addEventListener("click",function(e){
+    e.preventDefault()
+    addComment()
+})
+
+
+function plusCounter(){
+    let counterInt = parseInt(counter.textContent)
+    counterInt++
+    counter.textContent = counterInt
+}
+
+function minusCounter(){
+    let counterInt = parseInt(counter.textContent)
+    counterInt--
+    counter.textContent = counterInt
+}
+
+function likesCounter(){
+    const likesContainer = document.querySelector(".likes")
+    const li = document.createElement("li")
+    li.setAttribute("id",counter.textContent)
+    const exists = likesContainer.children.namedItem(`${counter.textContent}`)
+
+    if(exists){
+        exists.dataset.likes = parseInt(exists.dataset.likes) + 1
+        exists.textContent = `${exists.id} has been liked ${exists.dataset.likes}`
+    }else{
+        li.dataset.likes = 1
+        li.textContent = `${li.id} has been liked ${li.dataset.likes}`
+        likesContainer.append(li)
+    }
+    }
+
+    function pauseEverything(){
+
+        if(plus.disabled == pauseBtn){
+            plus.disabled = !pauseBtn
+            minus.disabled = !pauseBtn
+            heart.disabled = !pauseBtn
+            flag = true
+            pause.textContent = "resume"
+        }
+        else{
+            plus.disabled = pauseBtn
+            minus.disabled = pauseBtn
+            heart.disabled = pauseBtn
+            flag = false
+            pause.textContent = "pause"
+        }
+
+    }
+function counterInc(){
+
+        let counterInt = parseInt(counter.textContent)
+        if(flag == false){
+        counterInt++
+        counter.textContent = counterInt}
+    }
+
+
+function addComment(){
+    const p = document.createElement("p")
+    p.textContent = commentForm.comment.value
+    comments.append(p)
+} 
